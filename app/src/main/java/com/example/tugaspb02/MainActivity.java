bf0225d7427eb79ac3c4b2f86e7b6eac0841b490
GiffarySuwandi-50 <udagiffa@gmail.com>
2024-03-03 22:38:35 +0700

    Tugas Pemrograman Bergerak 02 (branch atasan)

--- app/src/main/java/com/example/tugaspb02/MainActivity.java
+++ app/src/main/java/com/example/tugaspb02/MainActivity.java
@@ -5,34 +5,54 @@ import android.os.Bundle;
 import android.view.View;
 
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private CheckBox cbReg;
-    private CheckBox cbDry;
-    private CheckBox cbExpo;
+    private RadioGroup radioGroupProducts;
+    private RadioButton rbReg;
+    private RadioButton rbDry;
+    private RadioButton rbExpo;
     private RadioButton rbMembership;
     private Button btnPesan;
+
+    private String produkTerpilih;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        cbReg = findViewById(R.id.cbReg);
-        cbDry = findViewById(R.id.cbDry);
-        cbExpo = findViewById(R.id.cbExpo);
+        radioGroupProducts = findViewById(R.id.radioGroupProducts);
+        rbReg = findViewById(R.id.rbReg);
+        rbDry = findViewById(R.id.rbDry);
+        rbExpo = findViewById(R.id.rbExpo);
         rbMembership = findViewById(R.id.rbMembership);
         btnPesan = findViewById(R.id.btnPesan);
+
         btnPesan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 prosesTransaksi();
+            }
+        });
+
+        radioGroupProducts.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                int idRadioButtonTerpilih = radioGroupProducts.getCheckedRadioButtonId();
+
+                if (idRadioButtonTerpilih == R.id.rbReg) {
+                    produkTerpilih = "Regular Service";
+                } else if (idRadioButtonTerpilih == R.id.rbDry) {
+                    produkTerpilih = "Dry Cleaning Service";
+                } else if (idRadioButtonTerpilih == R.id.rbExpo) {
+                    produkTerpilih = "Express-o! Service";
+                }
             }
         });
     }
@@ -48,9 +68,9 @@ public class MainActivity extends AppCompatActivity {
 
     private double kalkulasiBiayaAdmin() {
         double totalBiayaAdmin = 0;
-        if (cbReg.isChecked()) { totalBiayaAdmin += biayaAdmin("Regular Service"); }
-        if (cbDry.isChecked()) { totalBiayaAdmin += biayaAdmin("Dry Cleaning Service"); }
-        if (cbExpo.isChecked()) { totalBiayaAdmin += biayaAdmin("Express-o! Service"); }
+        if (rbReg.isChecked()) { totalBiayaAdmin += biayaAdmin("Regular Service"); }
+        if (rbDry.isChecked()) { totalBiayaAdmin += biayaAdmin("Dry Cleaning Service"); }
+        if (rbExpo.isChecked()) { totalBiayaAdmin += biayaAdmin("Express-o! Service"); }
         return totalBiayaAdmin;
     }
 
@@ -65,9 +85,9 @@ public class MainActivity extends AppCompatActivity {
     }
     private int jumlahBarang() {
         int kuantitasBarang = 0;
-        if (cbReg.isChecked()) { kuantitasBarang++;}
-        if (cbDry.isChecked()) { kuantitasBarang++;}
-        if (cbExpo.isChecked()) { kuantitasBarang++;}
+        if (rbReg.isChecked()) { kuantitasBarang++;}
+        if (rbDry.isChecked()) { kuantitasBarang++;}
+        if (rbExpo.isChecked()) { kuantitasBarang++;}
         return kuantitasBarang;
     }
 
@@ -77,29 +97,42 @@ public class MainActivity extends AppCompatActivity {
 
         // Bon Pemesanan Layanan
         String bon = "===== Receipt =====\nPelayanan yang anda pilih\n";
-        if (cbReg.isChecked()) { bon += "Regular Service" + "\n"; }
-        if (cbDry.isChecked()) { bon += "Dry Cleaning Service" + "\n"; }
-        if (cbExpo.isChecked()) { bon += "Express-o! Service" + "\n"; }
+
+        if (produkTerpilih != null) {
+            double hargaLayananTerpilih = hargaLayanan(produkTerpilih);
+            bon += produkTerpilih + " - Harga: " + hargaLayananTerpilih + "\n";
+        }
+
         bon += "Banyak Barang: " + jumlahBarang() + "\n";
 
         double totalHarga = ambilTotalHargaLayanan();
-        double totalDiskon = totalHarga * diskon;
-        double hargaAkhir = totalHarga + feeAdmin - totalDiskon;
+        double totalBiayaAdmin = feeAdmin;
 
-        bon += "Biaya Admin: " + feeAdmin +
+        // Hitung total harga barang + biaya admin
+        double totalHargaBarangDanBiayaAdmin = totalHarga + totalBiayaAdmin;
+
+        // Hitung diskon
+        double totalDiskon = totalHargaBarangDanBiayaAdmin * diskon;
+
+        // Hitung total setelah diskon
+        double totalSetelahDiskon = totalHargaBarangDanBiayaAdmin - totalDiskon;
+
+        bon += "Biaya Admin: " + totalBiayaAdmin +
                 "\nDiskon: " + (diskon * 100) + "%" +
                 "\nPemotongan: " + totalDiskon +
-                "\nTotal Bayar: " + hargaAkhir;
+                "\nTotal Bayar: " + totalSetelahDiskon;
 
         tunjukkanStrukBon(bon);
     }
 
+
+
     private double ambilTotalHargaLayanan() {
         double totalHargaProduk = 0;
 
-        if (cbReg.isChecked()) { totalHargaProduk += hargaLayanan("Regular Service");  }
-        if (cbDry.isChecked()) { totalHargaProduk += hargaLayanan("Dry Cleaning Service"); }
-        if (cbExpo.isChecked()) { totalHargaProduk += hargaLayanan("Express-o! Service");  }
+        if (rbReg.isChecked()) { totalHargaProduk += hargaLayanan("Regular Service");  }
+        if (rbDry.isChecked()) { totalHargaProduk += hargaLayanan("Dry Cleaning Service"); }
+        if (rbExpo.isChecked()) { totalHargaProduk += hargaLayanan("Express-o! Service");  }
         return totalHargaProduk;
     }
 
